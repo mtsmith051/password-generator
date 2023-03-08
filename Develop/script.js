6064f05b62798aef7d2eee497db7d5b8cc7f4781
mtsmith051 <mtsmith051@gmail.com>
2023-03-07 22:14:47 -0600

    Adds text to README.md; Adds code to 'script.js' file

--- Develop/script.js
+++ Develop/script.js
@@ -13,6 +13,36 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-function generatePassword () {
+var passArray = [];
+var upperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+var lowerCase = ["abcdefghijklmnopqrstuvwxyz"];
+var specialChars = ["!@#$%^&*()-=_+~\|/?,<.>"];
+var numeric = ["1234567890"];
 
-};
+function generatePassword() {
+  getPassLength();
+  getUpperCase();
+  getLowerCase();
+  getNumeric();
+  getSpecialChars();
+}
+
+function getPassLength() {
+  var inquiry = prompt ("Choose a password length");
+}
+
+function getUpperCase() {
+  var inquiry = confirm ("Do you want Uppercase letters?");
+}
+
+function getLowerCase() {
+  var inquiry = confirm ("Do you want Lowercase letters?");
+}
+
+function getNumeric() {
+  var inquiry = confirm ("Do you want Numbers?");
+}
+
+function getSpecialChars() {
+  var inquiry = confirm ("Do you want Special Characters?");
+}
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # password-generator
+
+## Description
+This project creates a function that will randomly create a password.
+
+### Index
